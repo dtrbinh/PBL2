a67796c3ad332548d5dbcc6b981903e2348ee27e
LePhiDuy <duyphi2011@gmail.com>
2021-11-03 18:03:53 +0700

    class

---
+++ Code/CongDan.h
@@ -0,0 +1,22 @@
+#ifndef CONGDAN_H
+#define CONGDAN_H
+#include <iostream>
+#include <string.h>
+using namespace std;
+
+class CONGDAN
+{
+    int ma_CD;
+    string fullName;
+    bool sex;
+    string phone;
+    string so_BHXH;
+    string so_CMND;
+    string address;
+    string ngay_DK;
+    string ma_DK;
+    string noi_DK;
+    bool mui1, mui2;
+    string ngay_M1, ngay_M2;
+};
+#endif

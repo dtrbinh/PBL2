0ca8b49a0aa126b7b9e7c7ff289da308e1997af5
LePhiDuy <duyphi2011@gmail.com>
2021-11-07 16:08:22 +0700

    header

--- Code/DanhSachCongDan.h
+++ Code/DanhSachCongDan.h
@@ -6,27 +6,44 @@ class DSCD
 {
     int n;
     CongDan *data = new CongDan[n];
-    static int count;
     public:
-        // Kết quả có được dữ liệu 3 file
+        // Hàm đọc file
         void nhapFile(string fileName1, string fileName2, string fileName3);
 
-        // Kết quả có dữ liệu của danh sách
-        void nhapTay(int n, CongDan CD);
+        // Hàm nhập dữ liệu bằng tay
+        void nhapTay(int n, CongDan CD[]);
 
-        // return danh sách mới
-        void Them(CongDan CD, int index);
+        // Thêm 1 công dân vào danh sách tại vị trí bất kì
+        void Them(CongDan CD[], CongDan cd, int index);
         
-        // Chèn vào vt bất kì => danh sách mới giữ nguyên thứ tự tăng/ giảm
-        void Insert(CongDan CD);
+        // kiểm tra tăng dần
+        template <typename T>
+        bool ascending(T left, T right)
+        {
+            return left > right;
+        }
 
-        // Trả về danh sách (mã CD, tên)
-        int Search();
+        // kiểm tra giảm dần
+        template <typename T>
+        bool descending(T left, T right)
+        {
+            return left < right;
+        }
 
-        // Sắp xếp = > danh sách mới
-        void Sort();
+        // Chèn công dân vào vị trí bất kì mà vẫn giữ nguyên thứ tự
+        void Insert(CongDan CD[], CongDan cd, T tieuchi,  bool (*func_ptr)(T, T) = ascending);
 
+        // Hàm tìm kiếm công dân
+        template <typename T>
+        CongDan Search(CongDan CD[], T tieuchi);
+        
+        // Sắp xếp
+        template <typename T>
+        void Sort(CongDan CD[], int n, T tieuchi, bool (*func_ptr)(T, T) = ascending);
         // 
+        void Delete();
 
 };
 #endif
+template <typename T>
+void swap(int &num1, int &num2);
